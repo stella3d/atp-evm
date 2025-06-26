48f22b531d1223a17bf673b9a076d86a9ee52998
Stella <stella@ondora.xyz>
2025-06-26 10:21:05 -0400

    add build

---
+++ dist/assets/cursor-CFPzvLDg.js
@@ -0,0 +1,3 @@
+import{m as o}from"./core-BtGtFV_K.js";import"./index-6D060j6a.js";import"./index.es-CDS_84D4.js";import"./events-DQ172AOg.js";const p=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{p as cursorSvg};
