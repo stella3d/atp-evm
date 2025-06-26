bb047771d50e11ae47c43b99f79e5c1d073c63cc
Stella <stella@ondora.xyz>
2025-06-26 11:25:44 -0400

    remove oauth fetch

--- src/oauth.ts
+++ src/oauth.ts
@@ -3,19 +3,22 @@ import { BrowserOAuthClient, type OAuthClientMetadataInput } from '@atproto/oaut
 const prodDomain = 'https://wallet-link.stellz.club';
 const isProduction = import.meta.env.PROD;
 
-const getConfig = async (): Promise<undefined | Readonly<OAuthClientMetadataInput>> => {
-  if (isProduction) {
-    const res = await fetch(`${prodDomain}/client-metadata.json`);
-    if (!res.ok) {
-      throw new Error(`Failed to fetch client metadata: ${res.status} ${res.statusText}`);
-    }
-    return await res.json() as OAuthClientMetadataInput;
-  } else { 
-    return undefined; 
-  }
-}
+const config: Readonly<OAuthClientMetadataInput> | undefined = isProduction ? {
+  "client_id": `${prodDomain}/client-metadata.json`,
+  "client_name": "Atproto Wallet Linker",
+  "client_uri": prodDomain,
+  "logo_uri": `${prodDomain}/logo.png`,
+  "tos_uri": `${prodDomain}/tos`,
+  "policy_uri": `${prodDomain}/policy`,
+  "redirect_uris": [prodDomain],
+  "scope": "atproto transition:generic",
+  "grant_types": ["authorization_code", "refresh_token"],
+  "response_types": ["code"],
+  "token_endpoint_auth_method": "none",
+  "application_type": "web",
+  "dpop_bound_access_tokens": true
+} : undefined;
 
-const config = await getConfig();
 export const oauthClient = new BrowserOAuthClient({
   clientMetadata: config,
   handleResolver: "https://bsky.social",
