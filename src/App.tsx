fa00672ec18ddcfc01378575e37f175c7d43db01
Stella <stella@ondora.xyz>
2025-06-26 12:09:12 -0400

    update terms and colors

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ function App() {
   return (
     <>
       <h1>ATProto Wallet Linker</h1>
-      <b>Only use this if you are OK with being publicly associated to the linked wallet.</b>
-      <p>A security & privacy guide will accompany a production release.</p>
+      <p>This allows you to link an Ethereum wallet address to your <a href="https://atproto.com/specs/did">ATProto DID</a> in a way that can be cryptographically verified on both sides.</p>
+      <b style={{ color: "#ee5f90" }}>Only use this if you are OK with being publicly associated to the linked wallet.</b>
+      <p>It works by writing a record to your PDS that contains the wallet address and a Sign In With Ethereum message from the wallet.</p>
       <br/>
       <AuthLinker />
     </>
