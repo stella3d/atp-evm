7c7b007cfb61e37e6f3143597a9e94bfe7a6a9f6
Stella <stella@ondora.xyz>
2025-07-02 21:47:45 -0400

    use more identity terms

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1>ATProto Wallet Linker</h1>
-      <p>This allows you to link an Ethereum wallet address to your <a href="https://atproto.com/specs/did">ATProto DID</a> in a way that can be cryptographically verified on both sides.</p>
+      <p>This allows you to link an Ethereum wallet address to your <a href="https://atproto.com/specs/did">ATProto identity</a> in a way that can be cryptographically verified on both sides.</p>
       <b style={{ color: "#ee5f90" }}>Only use this if you are OK with being publicly associated to the linked wallet.</b>
       <p>It works by writing a record to your PDS that contains the wallet address and a Sign In With Ethereum message from the wallet.</p>
       <br/>
