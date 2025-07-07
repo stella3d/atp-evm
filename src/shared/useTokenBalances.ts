b2ea83da505fadd77bdbf8a489e31a17af4511c0
Stella <stella@ondora.xyz>
2025-07-07 12:37:40 -0400

    add DAI on more chains

--- src/shared/useTokenBalances.ts
+++ src/shared/useTokenBalances.ts
@@ -18,18 +18,37 @@ const prefix = isProd ? '' : '/public';
 
 const ETH_LOGO_URL = `${prefix}/token_logos/eth.png`;
 const USDC_LOGO_URL = `${prefix}/token_logos/usdc.png`;
+const EURC_LOGO_URL = `${prefix}/token_logos/eurc.png`;
 const USDT_LOGO_URL = `${prefix}/token_logos/usdt.png`;
 const DAI_LOGO_URL = `${prefix}/token_logos/dai.png`;
+
+const DAI_SHARED_INFO = {
+  symbol: 'DAI',
+  name: 'Dai Stablecoin',
+  decimals: 18,
+  logoUrl: DAI_LOGO_URL
+};
+
+const USDC_SHARED_INFO = {
+  symbol: 'USDC',
+  name: 'USD Coin',
+  decimals: 6,
+  logoUrl: USDC_LOGO_URL
+};
+
+const EURC_SHARED_INFO = {
+  symbol: 'EURC',
+  name: 'Euro Coin',
+  decimals: 6,
+  logoUrl: EURC_LOGO_URL
+};
 
 // Common ERC20 tokens for each chain (you can expand this)
 const COMMON_TOKENS: Record<number, Array<{address: `0x${string}`, symbol: string, name: string, decimals: number, logoUrl: string}>> = {
   1: [ // Ethereum mainnet
     { 
       address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', 
-      symbol: 'USDC', 
-      name: 'USD Coin',
-      decimals: 6,
-      logoUrl: USDC_LOGO_URL
+      ...USDC_SHARED_INFO
     },
     { 
       address: '0xdAC17F958D2ee523a2206206994597C13D831ec7', 
@@ -40,42 +59,27 @@ const COMMON_TOKENS: Record<number, Array<{address: `0x${string}`, symbol: strin
     },
     { 
       address: '0x6B175474E89094C44Da98b954EedeAC495271d0F', 
-      symbol: 'DAI', 
-      name: 'Dai Stablecoin',
-      decimals: 18,
-      logoUrl: DAI_LOGO_URL
+      ...DAI_SHARED_INFO
     },
-	{
-		address: '0x1aBaEA1f7C830bD89Acc67eC4af516284b1bC33c',
-		symbol: 'EURC',
-		name: 'Euro Coin',
-		decimals: 6,
-		logoUrl: `${prefix}/token_logos/eurc.png`
-	}
+    {
+		  address: '0x1aBaEA1f7C830bD89Acc67eC4af516284b1bC33c',
+		  ...EURC_SHARED_INFO
+	  }
   ],
   8453: [ // Base
     { 
       address: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913', 
-      symbol: 'USDC', 
-      name: 'USD Coin',
-      decimals: 6,
-      logoUrl: USDC_LOGO_URL
+      ...USDC_SHARED_INFO
     },
-    { 
-      address: '0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed', 
-      symbol: 'DEGEN', 
-      name: 'Degen',
-      decimals: 18,
-      logoUrl: 'https://assets.coingecko.com/coins/images/34515/small/degen.png'
-    },
+    {
+      address: '0x60a3e35cc302bfa44cb288bc5a4f316fdb1adb42',
+      ...EURC_SHARED_INFO
+    }
   ],
   10: [ // Optimism
     { 
       address: '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85', 
-      symbol: 'USDC', 
-      name: 'USD Coin',
-      decimals: 6,
-      logoUrl: USDC_LOGO_URL
+      ...USDC_SHARED_INFO
     },
     { 
       address: '0x94b008aA00579c1307B0EF2c499aD98a8ce58e58', 
@@ -84,14 +88,15 @@ const COMMON_TOKENS: Record<number, Array<{address: `0x${string}`, symbol: strin
       decimals: 6,
       logoUrl: USDT_LOGO_URL
     },
+    {
+      address: '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1',
+      ...DAI_SHARED_INFO
+    }
   ],
   42161: [ // Arbitrum
     { 
       address: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831', 
-      symbol: 'USDC', 
-      name: 'USD Coin',
-      decimals: 6,
-      logoUrl: USDC_LOGO_URL
+      ...USDC_SHARED_INFO
     },
     { 
       address: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9', 
@@ -100,6 +105,10 @@ const COMMON_TOKENS: Record<number, Array<{address: `0x${string}`, symbol: strin
       decimals: 6,
       logoUrl: USDT_LOGO_URL
     },
+    {
+      address: '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1',
+      ...DAI_SHARED_INFO
+    }
   ],
 };
 
