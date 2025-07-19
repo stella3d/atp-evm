db9332291dae00075bcc326262902001a4aa99ec
Stella <stella@ondora.xyz>
2025-07-19 02:27:46 -0500

    remove 0.0.0.0 host

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: { 
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['wallet-link.stellz.club', '127.0.0.1', '0.0.0.0'] 
+    allowedHosts: ['wallet-link.stellz.club', '127.0.0.1'] 
   },
   build: {
     rollupOptions: {
